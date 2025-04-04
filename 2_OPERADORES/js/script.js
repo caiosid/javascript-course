efbf13f7e97e74f49a7053ac51079c4fe3a7d8a9
caiosid <caiosidartha8@gmail.com>
2025-04-04 08:36:15 -0300

    special numbers

--- 2_OPERADORES/js/script.js
+++ 2_OPERADORES/js/script.js
@@ -9,3 +9,9 @@ console.log(10 - 5)
 console.log(5 * 4)
 console.log(10 / 2)
 console.log(5 + (4 * 2))
+
+// 3 - Special Numbers
+console.log(typeof Infinity)
+console.log(typeof -Infinity)
+console.log(12 * "abc")
+console.log(typeof NaN)
