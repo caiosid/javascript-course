92bf228d23592c33fe6f95737ba132c378293450
caiosid <caiosidartha8@gmail.com>
2025-04-04 08:31:12 -0300

    operações aritméticas

--- 2_OPERADORES/js/script.js
+++ 2_OPERADORES/js/script.js
@@ -2,3 +2,10 @@
 console.log(typeof 2)
 console.log(typeof 5.14)
 console.log(typeof -127)
+
+// 2 - Ops. aritméticas
+console.log(2 + 4)
+console.log(10 - 5)
+console.log(5 * 4)
+console.log(10 / 2)
+console.log(5 + (4 * 2))
