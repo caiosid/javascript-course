3dd493832b7465fa8bb76c037a3d4f91f35c74e5
caiosid <caiosidartha8@gmail.com>
2025-04-09 13:59:25 -0300

    mais sobre arrays

--- 5_ARRAYS_OBJS/js/scripts.js
+++ 5_ARRAYS_OBJS/js/scripts.js
@@ -1,5 +1,5 @@
 // 1 - arrays
-const lista = [1,2,3,4,5];
+const lista = [1, 2, 3, 4, 5];
 
 console.log(lista);
 
@@ -8,3 +8,23 @@ console.log(typeof lista);
 const itens = ["Caio", "Cesar", "Sidartha", "Barbosa"];
 
 console.log(itens[1]);
+
+// 2 - mais sobre arrays
+const arr = ["a", "b", "c", "d", "e"];
+
+console.log(arr[0]);
+
+console.log(arr[2]);
+
+console.log(arr[84]);
+
+// 3 - propriedades
+const numbers = [5, 2, 6, 8];
+
+console.log(numbers.length);
+
+console.log(numbers["length"]);
+
+const myName = "Caio";
+
+console.log(myName.length);
