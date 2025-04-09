4789846a5b4cdfc41ed7479052c9d3f6ee4b06fc
caiosid <caiosidartha8@gmail.com>
2025-04-09 08:44:03 -0300

    parametro opicional

--- 4_FUNCOES/js/script.js
+++ 4_FUNCOES/js/script.js
@@ -109,3 +109,30 @@ console.log(raizQuadrada2(12));
 const helloWorld = () => console.log("Hello World!");
 
 helloWorld();
+
+// 7 - parametro opicional
+
+const multiplication = function (m, n) {
+  if (n === undefined) {
+    return m * 2;
+  } else {
+    return m * n;
+  }
+};
+
+console.log(multiplication(5));
+
+console.log(multiplication(2, 4));
+
+const greeting = (name) => {
+  if (!name) {
+    console.log("Olá");
+    return;
+  }
+
+  console.log(`Olá, ${name}`);
+};
+
+greeting();
+
+greeting("Caio");
