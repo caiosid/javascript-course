728eb2897141b9de6d2338d77807c37fdbb22333
caiosid <caiosidartha8@gmail.com>
2025-04-09 00:08:24 -0300

    criando função

---
+++ 4_FUNCOES/js/script.js
@@ -0,0 +1,23 @@
+// 1 -  criando uma função
+function minhaFuncao() {
+    console.log("Testando");
+}
+
+minhaFuncao();
+minhaFuncao();
+
+const minhaFuncaoEmVariavel = function () {
+    console.log("Função em variável");
+};
+
+minhaFuncaoEmVariavel();
+
+function funcaoComParamentro (txt) {
+    console.log(`Imprimindo ${txt}`);
+}
+
+funcaoComParamentro("Imprimindo alguma coisa");
+
+funcaoComParamentro("outra função");
+
+ 
