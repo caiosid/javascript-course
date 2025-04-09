dacf4b0f5d15e10bcfe8dfe09ac49e92a99c9205
caiosid <caiosidartha8@gmail.com>
2025-04-09 00:20:26 -0300

    retornando valores

--- 4_FUNCOES/js/script.js
+++ 4_FUNCOES/js/script.js
@@ -1,23 +1,37 @@
 // 1 -  criando uma função
 function minhaFuncao() {
-    console.log("Testando");
+  console.log("Testando");
 }
 
 minhaFuncao();
 minhaFuncao();
 
 const minhaFuncaoEmVariavel = function () {
-    console.log("Função em variável");
+  console.log("Função em variável");
 };
 
 minhaFuncaoEmVariavel();
 
-function funcaoComParamentro (txt) {
-    console.log(`Imprimindo ${txt}`);
+function funcaoComParamentro(txt) {
+  console.log(`Imprimindo ${txt}`);
 }
 
 funcaoComParamentro("Imprimindo alguma coisa");
 
 funcaoComParamentro("outra função");
 
- 
+// 2 - Retornando valores das funções
+const a = 10;
+const b = 20;
+const c = 30;
+const d = 40;
+
+function soma(n1, n2) {
+  return n1 + n2;
+}
+
+const resultado = soma(a, b);
+
+console.log(resultado);
+
+console.log(soma(c, d));
