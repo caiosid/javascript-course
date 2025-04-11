be87c4ebdbbfc7446623b629d974aff43ed5a073
caiosid <caiosidartha8@gmail.com>
2025-04-11 10:19:01 -0300

    aprofundando em métodos

--- 6_ORIENTACAO-OBJETO/js/script.js
+++ 6_ORIENTACAO-OBJETO/js/script.js
@@ -9,3 +9,22 @@ const animal = {
 console.log(animal.nome);
 
 animal.latir();
+
+// 2 - aprofundando em métodos
+const pessoa = {
+  nome: "Caio",
+  getNome: function () {
+    return this.nome;
+  },
+
+  setNome: function (novoNome) {
+    this.nome = novoNome;
+  },
+};
+
+console.log(pessoa.nome);
+console.log(pessoa.getNome());
+
+pessoa.setNome("Cesar");
+
+console.log(pessoa.getNome());
