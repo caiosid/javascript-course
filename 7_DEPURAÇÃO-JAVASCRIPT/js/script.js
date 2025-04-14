8dd8a377714c840eb44ca13d1486155960836c5e
caiosid <caiosidartha8@gmail.com>
2025-04-14 14:07:44 -0300

    finally

--- 7_DEPURAÇÃO-JAVASCRIPT/js/script.js
+++ 7_DEPURAÇÃO-JAVASCRIPT/js/script.js
@@ -75,3 +75,16 @@ try {
 } catch (error) {
   console.log(`Erro no programa: ${error}`);
 }
+
+// 7 - finally
+try {
+  const value = checkNumber("asd");
+
+  if (!value) {
+    throw new Error("Valores inválidos");
+  }
+} catch (error) {
+  console.log(`Opa, aconteceu um problema: ${error}`);
+} finally {
+  console.log("O código foi excecutado!");
+}
