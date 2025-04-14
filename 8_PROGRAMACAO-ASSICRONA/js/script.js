75484846b843d8a5b574962014d4816ee400bdd5
caiosid <caiosidartha8@gmail.com>
2025-04-14 14:37:08 -0300

    setInternal

--- 8_PROGRAMACAO-ASSICRONA/js/script.js
+++ 8_PROGRAMACAO-ASSICRONA/js/script.js
@@ -1,8 +1,20 @@
 // 1 - setTimout
 console.log("Ainda não executou");
 
-setTimeout(function ()  {
-    console.log("Requisão assíncrona");
+setTimeout(function () {
+  console.log("Requisão assíncrona");
 }, 200);
 
 console.log("Ainda não executou 2");
+
+// 2 - setInternal
+//console.log("Ainda não começou");
+
+//setInterval(function () {
+  //console.log("Intervalo assíncrono");
+//}, 3000);
+
+//console.log("Ainda não começou 2");
+
+
+
