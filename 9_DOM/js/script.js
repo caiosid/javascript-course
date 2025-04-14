dda4b3c6847b262dbbb4ad0107667e9aa5eec207
caiosid <caiosidartha8@gmail.com>
2025-04-14 17:38:08 -0300

    selecionando por tags

--- 9_DOM/js/script.js
+++ 9_DOM/js/script.js
@@ -6,3 +6,8 @@ console.log(document.body.childNodes[1]);
 console.log(document.body.childNodes[1].childNodes);
 
 console.log(document.body.childNodes[1].childNodes[1].textContent);
+
+// 2 - selecionando por tags
+const listItens = document.getElementsByTagName("li");
+
+console.log(listItens);
