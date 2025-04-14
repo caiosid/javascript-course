a4ac231ceaf3ac41b8047c3e6542b647aa66084a
caiosid <caiosidartha8@gmail.com>
2025-04-14 14:48:58 -0300

    promise

--- 8_PROGRAMACAO-ASSICRONA/js/script.js
+++ 8_PROGRAMACAO-ASSICRONA/js/script.js
@@ -11,10 +11,22 @@ console.log("Ainda não executou 2");
 //console.log("Ainda não começou");
 
 //setInterval(function () {
-  //console.log("Intervalo assíncrono");
+//console.log("Intervalo assíncrono");
 //}, 3000);
 
 //console.log("Ainda não começou 2");
 
+// 3 - promise
+const promessa = Promise.resolve(5 + 5);
 
+console.log("Algum código");
 
+promessa
+  .then((value) => {
+    console.log(`A soma é ${value}`);
+    return value;
+  })
+  .then((value) => value - 1)
+  .then((value) => console.log(`Agora é ${value}`));
+
+console.log("Algum código");
